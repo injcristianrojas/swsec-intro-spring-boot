45d687ba337fb2b62a16f553adabd363493d1218
Cristián Rojas <injcristianrojas@gmail.com>
2018-11-05 14:26:38 -0300

    Working base example

--- src/main/java/cl.injcristianrojas/HomeController.java
+++ src/main/java/cl.injcristianrojas/HomeController.java
@@ -1,14 +1,19 @@
 package cl.injcristianrojas;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(Model model) {
-        model.addAttribute("serverTime", "test" );
-        return "home";
+    public String index(Model model) {
+        model.addAttribute("serverTime", ZonedDateTime.now(ZoneId.of("America/Santiago")));
+        return "index";
     }
 
 }
