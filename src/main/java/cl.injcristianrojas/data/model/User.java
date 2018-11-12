ded04637244e86c6354a7a2c668067f3d8ccd8ce
Cristián Rojas <injcristianrojas@gmail.com>
2018-11-12 17:12:46 -0300

    Auth looks OK

--- src/main/java/cl.injcristianrojas/data/model/User.java
+++ src/main/java/cl.injcristianrojas/data/model/User.java
@@ -11,6 +11,8 @@ public class User {
     @GeneratedValue
     private Long id;
     private String username;
+    private String password;
+    private boolean enabled = true;
 
     public String getUsername() {
         return username;
@@ -18,5 +20,29 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 }
--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user VALUES(10001, 'admin');
-INSERT INTO user VALUES(10002, 'jperez');
-INSERT INTO user VALUES(10003, 'jbolsonaro');
-INSERT INTO user VALUES(10004, 'dtrump');
