6e873912048d223feaaf3a34a074a56b687cc87a
Cristián Rojas <injcristianrojas@gmail.com>
2018-11-26 19:46:34 -0300

    Proper implementation of save

--- src/main/java/cl.injcristianrojas/data/repositories/PostRepository.java
+++ src/main/java/cl.injcristianrojas/data/repositories/PostRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAll();
-    Post save(Post post);
+    <S extends Post> S save(S s);
 }
