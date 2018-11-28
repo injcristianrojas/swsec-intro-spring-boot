6eec7a5b199deb1a50ab67f43bc061ef1ffeff0d
Cristián Rojas <injcristianrojas@gmail.com>
2018-11-28 18:37:44 -0300

    More refactoring

--- src/main/java/cl/injcristianrojas/controllers/api/UserController.java
+++ src/main/java/cl/injcristianrojas/controllers/api/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class UserRestController {
+public class UserController {
 
     @Autowired
     private UserRepository userRepo;
