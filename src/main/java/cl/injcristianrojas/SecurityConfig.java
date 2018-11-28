bf02f469ce16c9fa601f9b41516e6f396f3f7861
Cristián Rojas <injcristianrojas@gmail.com>
2018-11-27 21:04:44 -0300

    Better logged in message

--- src/main/java/cl/injcristianrojas/SecurityConfig.java
+++ src/main/java/cl/injcristianrojas/SecurityConfig.java
@@ -5,7 +5,6 @@ import cl.injcristianrojas.security.AuthenticationSuccessHandlerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
