4e7c9d6ee22017c9759460919baa52d9d5b6119f
Cristián Rojas <injcristianrojas@gmail.com>
2018-12-10 18:50:48 -0300

    Back to JWTless config

--- src/main/java/cl/injcristianrojas/security/SecurityConfig.java
+++ src/main/java/cl/injcristianrojas/security/SecurityConfig.java
@@ -7,28 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import cl.injcristianrojas.data.service.MainUserDetailsService;
-import cl.injcristianrojas.data.service.JwtUserDetailsService;
-import cl.injcristianrojas.security.jwt.JwtAuthenticationFilter;
-import cl.injcristianrojas.security.jwt.JwtAuthorizationFilter;
-
-import static cl.injcristianrojas.security.jwt.Constants.SIGN_UP_URL;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
@@ -83,41 +73,21 @@ public class SecurityConfig {
 	@Order(1)
 	@Configuration
 	public static class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		private JwtUserDetailsService userDetailsService;
-		private PasswordEncoder passwordEncoder;
-
-		public ApiSecurityConfig(JwtUserDetailsService userDetailsService) {
-			this.userDetailsService = userDetailsService;
-			this.passwordEncoder = NoOpPasswordEncoder.getInstance();
-		}
-
+		
 		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-					.antMatcher("/api/**")
-					.cors().and().csrf().disable()
-					.authorizeRequests()
-					.antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-					.anyRequest().authenticated()
-					.and()
-					.addFilter(new JwtAuthenticationFilter(authenticationManager()))
-					.addFilter(new JwtAuthorizationFilter(authenticationManager()))
-					// this disables session creation on Spring Security
-					.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		}
-
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.
+	        		antMatcher("/api/**")
+	        		.cors()
+	        		.and()
+	        		.csrf()
+	        		.disable();
+	    }
+		
 		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-		}
-
-		@Bean
-		CorsConfigurationSource corsConfigurationSource() {
-			final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-			return source;
-		}
-
+        public void configure(WebSecurity web) throws Exception {
+            super.configure(web);
+        }
+		
 	}
 }
