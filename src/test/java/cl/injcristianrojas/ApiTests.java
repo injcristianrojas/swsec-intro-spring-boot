e0175f9ab3d4d9ccc824282c75a5a558d808d685
Cristián Rojas <injcristianrojas@gmail.com>
2018-12-10 21:31:34 -0300

    New JWT API Tests

---
+++ src/test/java/cl/injcristianrojas/ApiTests.java
@@ -0,0 +1,58 @@
+package cl.injcristianrojas;
+
+import static cl.injcristianrojas.security.jwt.Constants.TOKEN_PREFIX;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiTests {
+	
+	@Autowired
+	private MockMvc mvc;
+
+	private String jwtToken;
+	
+	@Before
+	public void setUp() throws Exception {
+		MvcResult result = this.mvc.perform(post("/api/login").param("Content-Type", "application/json").content("{ \"username\": \"jperez\", \"password\": \"123\"}")).andReturn();
+		jwtToken = result.getResponse().getHeader("Authorization").replace(TOKEN_PREFIX, "");
+	}
+	
+	@Test
+    public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
+        this.mvc.perform(get("/api/users")).andExpect(status().isForbidden());
+    }
+
+	@Test
+	public void testUsers() throws Exception {
+		this.mvc.perform(get("/api/users").header("Authorization", TOKEN_PREFIX + jwtToken))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[1].id", is(2)))
+				.andExpect(jsonPath("$[1].username", is("jperez")));
+				
+	}
+	
+	@Test
+	public void testPosts() throws Exception {
+		this.mvc.perform(get("/api/posts").header("Authorization", TOKEN_PREFIX + jwtToken))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id", is(1)))
+				.andExpect(jsonPath("$[0].message", is("Holi")));
+				
+	}
+}
