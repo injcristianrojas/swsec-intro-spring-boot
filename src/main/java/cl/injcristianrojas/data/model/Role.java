901ff067ced7758010460e9c8e45cca2f7d72750
Cristián Rojas <injcristianrojas@gmail.com>
2018-12-16 21:45:00 -0300

    Minor correction

--- src/main/java/cl/injcristianrojas/data/model/Role.java
+++ src/main/java/cl/injcristianrojas/data/model/Role.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Role {
 	
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 	private String rolename;
 
