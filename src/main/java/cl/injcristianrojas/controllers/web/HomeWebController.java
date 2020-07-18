7eea0209e6c490cbf34169eb5ee6dfd9ccd68473
Cristian Rojas <injcristianrojas@gmail.com>
2020-07-18 19:51:14 -0400

    Cosmetic fixes

--- src/main/java/cl/injcristianrojas/controllers/web/HomeWebController.java
+++ src/main/java/cl/injcristianrojas/controllers/web/HomeWebController.java
@@ -1,18 +1,18 @@
 package cl.injcristianrojas.controllers.web;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 @Controller
 public class HomeWebController {
 
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("serverTime", ZonedDateTime.now(ZoneId.of("America/Santiago")));
+        model.addAttribute("serverTime", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
         return "index";
     }
 
