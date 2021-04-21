05a33400dfd415c0848a55a915ffef0aa5f3404f
Cristian Rojas <injcristianrojas@gmail.com>
2021-04-21 17:42:49 -0400

    New form of web testing

--- src/test/java/cl/injcristianrojas/WebTests.java
+++ src/test/java/cl/injcristianrojas/WebTests.java
@@ -1,42 +1,37 @@
 package cl.injcristianrojas;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class WebTests {
+
 	@Autowired
 	private MockMvc mvc;
+	private RequestBuilder requestBuilder;
 
 	@Test
-	public void testHomePage() throws Exception {
-		this.mvc.perform(get("/login")).andExpect(status().is2xxSuccessful());
+	public void testWithoutLogin() throws Exception {
+		this.mvc.perform(get("/")).andExpect(content().string(not(containsString("lhamilton"))));
 	}
-	
+
 	@Test
+	@WithMockUser(username="lhamilton", password="123")
 	public void testLogin() throws Exception {
-		RequestBuilder requestBuilder = post("/login")
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-	            .param("username", "lhamilton")
-	            .param("password", "123")
-	            .with(csrf());
-		this.mvc.perform(requestBuilder)
-	    		.andExpect(redirectedUrl("/"))
-	            .andExpect(status().isFound());
+		this.mvc.perform(get("/")).andExpect(content().string(containsString("lhamilton")));
 	}
 }
