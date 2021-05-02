88168f0e48934d6ca2bef40734e7f5335748c774
Cristian Rojas <injcristianrojas@gmail.com>
2021-05-02 13:59:28 -0400

    Naming changes

---
+++ src/main/java/cl/injcristianrojas/AvesChilenasApplication.java
@@ -0,0 +1,14 @@
+package cl.injcristianrojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class AvesChilenasApplication extends SpringBootServletInitializer {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(AvesChilenasApplication.class, args);
+    }
+
+}
