0d3310373ea68810d9ecf0d1e7c42b53a70541cc
Cristian Rojas <injcristianrojas@gmail.com>
2021-05-02 14:02:23 -0400

    Main change

--- src/main/java/cl/injcristianrojas/AvesChilenasApplication.java
+++ src/main/java/cl/injcristianrojas/AvesChilenasApplication.java
@@ -2,10 +2,9 @@ package cl.injcristianrojas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AvesChilenasApplication extends SpringBootServletInitializer {
+public class AvesChilenasApplication {
 	
     public static void main(String[] args) {
         SpringApplication.run(AvesChilenasApplication.class, args);
