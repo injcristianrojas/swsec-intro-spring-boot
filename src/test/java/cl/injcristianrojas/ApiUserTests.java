bbd23b5cea5711e62bba80c2bbafcc898f532a89
Cristián Rojas <injcristianrojas@gmail.com>
2023-07-21 19:28:38 -0400

    Better token extraction on tests

--- src/test/java/cl/injcristianrojas/ApiUserTests.java
+++ src/test/java/cl/injcristianrojas/ApiUserTests.java
@@ -17,34 +17,38 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.jayway.jsonpath.JsonPath;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApiUserTests {
-	
+
 	@Autowired
 	private MockMvc mvc;
 
 	private String jwtToken;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		MvcResult result = this.mvc.perform(post("/api/v2/login").param("Content-Type", "application/json").content("{ \"username\": \"lhamilton\", \"password\": \"roscoe\"}")).andReturn();
-		jwtToken = result.getResponse().getHeader("Authorization").replace(TOKEN_PREFIX, "");
+		MvcResult result = this.mvc.perform(post("/api/v2/login").param("Content-Type", "application/json")
+				.content("{ \"username\": \"lhamilton\", \"password\": \"roscoe\"}")).andReturn();
+		jwtToken = JsonPath.read(result.getResponse().getContentAsString(), "$.token");
+
 	}
 
 	@Test
-    public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
-        this.mvc.perform(get("/api/v2/users")).andExpect(status().isForbidden());
-    }
+	public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
+		this.mvc.perform(get("/api/v2/users")).andExpect(status().isForbidden());
+	}
 
 	@Test
 	public void testUsers() throws Exception {
 		this.mvc.perform(get("/api/v2/users").header("Authorization", TOKEN_PREFIX + jwtToken))
 				.andExpect(status().isUnauthorized());
-				
+
 	}
-	
+
 	@Test
 	public void testPosts() throws Exception {
 		this.mvc.perform(get("/api/v2/posts").header("Authorization", TOKEN_PREFIX + jwtToken))
