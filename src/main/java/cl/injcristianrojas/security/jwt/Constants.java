722105093269f0cd56621ad089f004f8f70f17d8
Cristián Rojas <injcristianrojas@gmail.com>
2023-07-26 23:40:09 -0400

    JWT secret takes longer to crack now

--- src/main/java/cl/injcristianrojas/security/jwt/Constants.java
+++ src/main/java/cl/injcristianrojas/security/jwt/Constants.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 
 public class Constants {
 	
-	public static final String SECRET = "abc";
+	public static final String SECRET = "123456789012345678901234567890";
     public static final long EXPIRATION_TIME_IN_SECONDS = 86_400; // seconds
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
